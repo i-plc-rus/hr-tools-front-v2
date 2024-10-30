305cba40553b961268c9410e720bac9be2332838
Aynur <ghostgo81@gmail.com>
2024-10-31 01:03:24 +0400

    Перемещена таблица сотрудников из модуля администратора в модуль пользователя

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -6,47 +6,19 @@ import {RouterModule} from '@angular/router';
 import {routes} from './admin.routes';
 import {LayoutComponent} from '../../components/layout/layout.component';
 import {SidebarComponent} from '../../components/sidebar/sidebar.component';
-import {AddUserModalComponent} from './users/add-user-modal/add-user-modal.component';
-import {DeleteUserModalComponent} from './users/delete-user-modal/delete-user-modal.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {AgGridAngular} from 'ag-grid-angular';
-import {TableButtonComponent} from './users/table-button/table-button.component';
-import {TableSpinnerComponent} from './users/table-spinner/table-spinner.component';
-
-import {MatIcon, MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatTooltip} from '@angular/material/tooltip';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatIcon} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AdminComponent,
     UsersComponent,
-    TableButtonComponent,
-    TableSpinnerComponent,
-    AddUserModalComponent,
-    DeleteUserModalComponent,
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     LayoutComponent,
     SidebarComponent,
-    FormsModule,
-    ReactiveFormsModule,
-    AgGridAngular,
-
     MatIcon,
-    MatProgressSpinnerModule,
-    MatIconModule,
-    MatButtonModule,
-    MatTooltip,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
   ]
 })
 export class AdminModule {
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -2,7 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {routes} from './user.routes';
+import {AgGridAngular} from 'ag-grid-angular';
 import {UserComponent} from './user.component';
+import {UsersListComponent} from './users-list/users-list.component';
+import {TableButtonComponent} from './users-list/table-button/table-button.component';
+import {TableSpinnerComponent} from './users-list/table-spinner/table-spinner.component';
+import {AddUserModalComponent} from './users-list/add-user-modal/add-user-modal.component';
+import {DeleteUserModalComponent} from './users-list/delete-user-modal/delete-user-modal.component';
 import {VacancyListComponent} from './vacancy-list/vacancy-list.component';
 import {RequestListComponent} from './request-list/request-list.component';
 import {RequestCreationComponent} from './request-creation/request-creation.component';
@@ -17,11 +23,20 @@ import {MatIcon, MatIconModule} from "@angular/material/icon";
 import {MatTooltip} from "@angular/material/tooltip";
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 
 @NgModule({
   declarations: [
     UserComponent,
+    UsersListComponent,
+    TableButtonComponent,
+    TableSpinnerComponent,
+    AddUserModalComponent,
+    DeleteUserModalComponent,
     VacancyListComponent,
     RequestListComponent,
     RequestCreationComponent,
@@ -36,12 +51,17 @@ import {MatMenuModule} from '@angular/material/menu';
     FormsModule,
     ReactiveFormsModule,
     NotificationBubbleComponent,
+    AgGridAngular,
 
     MatIcon,
     MatIconModule,
     MatButtonModule,
     MatTooltip,
     MatMenuModule,
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
   ]
 })
 export class UserModule { }

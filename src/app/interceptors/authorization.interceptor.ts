956100b1bd6097272db2727434d67e2421fefdea
Aynur <35735068+ghostgo@users.noreply.github.com>
2024-12-04 18:33:24 +0400

    обновление jwt (#9)

--- src/app/interceptors/authorization.interceptor.ts
+++ src/app/interceptors/authorization.interceptor.ts
@@ -1,24 +1,43 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {HttpInterceptorFn, HttpRequest} from '@angular/common/http';
 import {inject} from '@angular/core';
 import {TokenService} from '../services/token.service';
-import {catchError, throwError} from 'rxjs';
+import {catchError, switchMap, throwError} from 'rxjs';
 import {UserService} from '../services/user.service';
 
 export const authorizationInterceptor: HttpInterceptorFn = (req, next) => {
   const tokenService = inject(TokenService);
   const userService = inject(UserService);
-  if (tokenService.token) {
-    req = req.clone({
-      headers: req.headers.set('Authorization', 'Bearer ' + tokenService.token)
-    });
+  function addAuthHeader(request: HttpRequest<unknown>) {
+    if (tokenService.token)
+      return request.clone({
+        headers: request.headers.set('Authorization', 'Bearer ' + tokenService.token)
+      });
+    return request;
   }
+
+  req = addAuthHeader(req);
+
   return next(req).pipe(
     catchError((error) => {
-      if ((error.status === 400 && error.error === 'missing or malformed JWT') || error.status === 401) {
-        console.log(error);
-        userService.logout();
-        //todo обновление токена
+      const isUnauthorized = (error.status === 400 && error.error === 'missing or malformed JWT') || error.status === 401;
+      if (isUnauthorized && !tokenService.isRefreshing && tokenService.refreshToken) {
+        tokenService.isRefreshing = true;
+        return tokenService.refresh()
+          .pipe(
+            switchMap((response) => {
+              tokenService.updateToken(response.body?.data?.token, response.body?.data?.refresh_token);
+              tokenService.isRefreshing = false;
+              req = addAuthHeader(req);
+              return next(req);
+            }),
+            catchError((e) => {
+              userService.logout();
+              tokenService.isRefreshing = false;
+              return throwError(() => new Error(JSON.stringify(e)));
+            })
+          );
       }
-      return throwError(() => new Error(JSON.stringify(error)));
+      else
+        return throwError(() => new Error(JSON.stringify(error)));
     }))
 };
--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,27 +1,44 @@
 import {Injectable} from '@angular/core';
 import {CookieService} from './cookie.service';
+import {ApiService} from '../api/Api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
   token = '';
+  refreshToken = '';
+  isRefreshing = false;
 
-  constructor(private cookieService: CookieService) {
+  constructor(
+    private cookieService: CookieService,
+    private api: ApiService
+  ) {
     this.updateToken();
   }
 
-  updateToken(token?: string) {
+  updateToken(token?: string, refreshToken?: string) {
     if (token) {
       this.cookieService.setCookie({name: 'hr-platform', value: token});
     }
+    if (refreshToken) {
+      localStorage.setItem('hr-platform-refresh', refreshToken);
+    }
     this.token = this.cookieService.getCookie('hr-platform');
+    this.refreshToken = localStorage.getItem('hr-platform-refresh') || '';
+  }
+
+  refresh() {
+    if (!this.refreshToken) {
+      this.refreshToken = localStorage.getItem('hr-platform-refresh') || '';
+    }
+    return this.api.v1AuthRefreshTokenCreate({refresh_token: this.refreshToken}, {observe: 'response'})
   }
 
   clearToken() {
     this.token = '';
+    this.refreshToken = '';
     this.cookieService.deleteCookie('hr-platform');
+    localStorage.removeItem('hr-platform-refresh');
   }
-
-  // todo надо реализовать менеджмент рефреш токена, да и вообще механизм обновления JWT токена, тк бэк сам это не делает
 }

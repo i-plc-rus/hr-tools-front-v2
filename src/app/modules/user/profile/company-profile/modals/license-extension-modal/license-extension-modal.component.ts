1f376f3ba91442232e0dc7a5e9f6fa5ca8967c4d
Ko.Kko <ko.kko@mrm.ru>
2025-04-21 08:07:40 +0300

    feat: HRTOOLS-41

--- src/app/modules/user/profile/company-profile/modals/license-extension-modal/license-extension-modal.component.ts
+++ src/app/modules/user/profile/company-profile/modals/license-extension-modal/license-extension-modal.component.ts
@@ -1,38 +1,83 @@
-import {Component, EventEmitter} from '@angular/core';
+import {Component, EventEmitter, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {UsersModalService} from '../../../../../../services/users-modal.service';
 import {SnackBarService} from '../../../../../../services/snackbar.service';
+import {catchError, finalize, of} from 'rxjs';
+import {SpaceapimodelsSalesRequest} from '../../../../../../api/data-contracts';
+import {ApiService} from '../../../../../../api/Api';
 
 @Component({
   selector: 'app-license-extension-modal',
   templateUrl: './license-extension-modal.component.html',
   styleUrl: './license-extension-modal.component.scss'
 })
-export class LicenseExtensionModalComponent {
+export class LicenseExtensionModalComponent implements OnInit{
   onSubmit = new EventEmitter<any>();
   isLoading = false;
 
   extensionForm = new FormGroup({
-    company: new FormControl('', [Validators.required]),
+    company: new FormControl({value: '', disabled: true}),
     comment: new FormControl('', [Validators.required])
   });
 
-  constructor(private modalService: UsersModalService, private snackBarService: SnackBarService) {
+  constructor(
+    private modalService: UsersModalService,
+    private snackBarService: SnackBarService,
+    private apiService: ApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadCompanyData();
   }
+
+  private loadCompanyData(): void {
+    this.apiService.v1SpaceProfileList({observe: 'response'}).subscribe({
+      next: (response) => {
+        if (response && response.body && response.body.data) {
+          const companyName = response.body.data.organization_name || '';
+          this.extensionForm.patchValue({
+            company: companyName
+          });
+        }
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
+
   submit() {
     if (!this.extensionForm.valid) return;
 
     this.isLoading = true;
+
     const formData = {
       company: this.extensionForm.controls.company.value,
       comment: this.extensionForm.controls.comment.value
     };
 
+    const requestBody: SpaceapimodelsSalesRequest = {
+      text: formData.comment || ''
+    };
 
-    this.onSubmit.emit(formData);
-    this.snackBarService.snackBarMessageSuccess('Заявка успешно отправлена!')
-    this.modalService.closeModal();
-    this.isLoading = false;
+    this.apiService.v1SpaceProfileSendLicenseRequestUpdate(requestBody)
+      .pipe(
+        catchError(error => {
+            this.snackBarService.snackBarMessageError(JSON.parse(error.message).error.message);
+          return of(null);
+        }),
+        finalize(() => {
+          this.isLoading = false;
+        })
+      )
+      .subscribe(response => {
+        if (response && response.status === 'success') {
+          this.snackBarService.snackBarMessageSuccess('Заявка успешно отправлена!');
+          this.onSubmit.emit(formData);
+          this.modalService.closeModal();
+          this.extensionForm.reset();
+        }
+      });
   }
 
   cancel() {
@@ -40,5 +85,4 @@ export class LicenseExtensionModalComponent {
     this.modalService.closeModal();
     this.extensionForm.reset();
   }
-
 }

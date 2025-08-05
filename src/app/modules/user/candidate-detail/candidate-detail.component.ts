1d7e115fbc38dced66677e143d5603266cb7d4ac
atali <mywork7539@mobilexapps.com>
2025-08-05 13:01:38 +0300

    hrtools-125

--- src/app/modules/user/candidate-detail/candidate-detail.component.ts
+++ src/app/modules/user/candidate-detail/candidate-detail.component.ts
@@ -1,11 +1,13 @@
 import {
   AfterViewInit,
   Component,
+  ElementRef,
   EventEmitter,
   Input,
   OnChanges, OnDestroy,
   OnInit,
   Output,
+  Renderer2,
   SimpleChanges,
   ViewChild
 } from '@angular/core';
@@ -23,6 +25,7 @@ import {
 import {ApplicantHistoryView} from '../../../models/ApplicantHistory';
 import {MatTabGroup} from '@angular/material/tabs';
 import {SnackBarService} from '../../../services/snackbar.service';
+import { PDFDocumentProxy, PdfViewerComponent } from 'ng2-pdf-viewer';
 
 @Component({
   selector: 'app-candidate-detail',
@@ -56,7 +59,8 @@ export class CandidateDetailComponent implements OnInit,AfterViewInit, OnChanges
     private api: ApiService,
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private snackBarService: SnackBarService
+    private snackBarService: SnackBarService,
+    private renderer: Renderer2,
   ) { }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -83,6 +87,19 @@ export class CandidateDetailComponent implements OnInit,AfterViewInit, OnChanges
         this.tabGroup.selectedIndex = this.selectedTabIndex;
       }
     });
+  }
+
+  afterLoadComplete(pdf: PDFDocumentProxy): void {
+    let totalHeight = 0;
+    for (let i = 1; i <= pdf.numPages; i++) {
+      pdf.getPage(i).then(page => {
+        const viewport = page.getViewport({ scale: 1.3333333333333333 });
+        totalHeight += viewport.height + 10
+        if (i === pdf.numPages) {
+          document.getElementById('pdfViewer')!.style.height = `${(totalHeight)}px`;
+        }
+      });
+    }
   }
 
   onTabChange(index: number) {

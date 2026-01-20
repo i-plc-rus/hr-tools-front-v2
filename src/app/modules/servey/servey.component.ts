fcb8ba211b4a8b3d68798f94fed1b75baad79b87
atali <mywork7539@mobilexapps.com>
2026-01-20 15:15:59 +0300

    part-1

--- src/app/modules/servey/servey.component.ts
+++ src/app/modules/servey/servey.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-servey',
   templateUrl: './servey.component.html',
-  styleUrl: './servey.component.scss'
+  styleUrl: './servey.component.scss',
 })
 export class ServeyComponent {
+  @HostListener('contextmenu', ['$event'])
+  onContextMenu(event: MouseEvent) {
+    event.preventDefault();
+  }
 
+  // Запрет события копирования
+  @HostListener('copy', ['$event'])
+  onCopy(event: ClipboardEvent) {
+    event.preventDefault();
+    return false;
+  }
+
+  // Запрет вырезания текста
+  @HostListener('cut', ['$event'])
+  onCut(event: ClipboardEvent) {
+    event.preventDefault();
+    return false;
+  }
 }
